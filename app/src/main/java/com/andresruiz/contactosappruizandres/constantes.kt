8fcf2a88b92afef841cff56b52c5d00faa65b580
Andrés Ruiz <franklin.ruiz@epn.edu.ec>
2020-07-01 15:59:56 -0500

    Encriptación y Uso memoria interna y Externa

--- app/src/main/java/com/andresruiz/contactosappruizandres/constantes.kt
+++ app/src/main/java/com/andresruiz/contactosappruizandres/constantes.kt
@@ -2,3 +2,5 @@ package com.andresruiz.contactosappruizandres
 
 const val LOGIN_KEY = "LOGIN_KEY"
 const val PASSWORD_KEY = "PASSWORD_KEY"
+const val SECRET_FILENAME = "secret_shared_prefs"
+const val CONTACTS_FILENAME = "contactsfile.dat"
